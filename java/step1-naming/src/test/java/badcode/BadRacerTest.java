324f154ec9f23880d78dc90041fea0190bca81d2
chlwlstj98 <c55822605@gmail.com>
2026-04-08 02:29:12 +0900

    [ECS-45] refactor: 테스트 DisplayName 및 변수명을 의미있는 이름으로 개선

--- java/step1-naming/src/test/java/badcode/BadRacerTest.java
+++ java/step1-naming/src/test/java/badcode/BadRacerTest.java
@@ -11,43 +11,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BadRacerTest {
 
-    @DisplayName("테스트1")
+    @DisplayName("랜덤 값이 4 이상이면 움직일 수 있다")
     @Test
-    void t1() {
+    void moveForward_whenRandomValueIsAboveThreshold() {
         BadRacer r = new BadRacer();
-        assertThat(r.doM(4)).isTrue();
+        assertThat(r.canMove(4)).isTrue();
     }
 
-    @DisplayName("테스트2")
+    @DisplayName("랜덤 값이 4 미만이면 움직일 수 없다")
     @Test
-    void t2() {
+    void cantMoveForward_whenRandomValueIsBelowThreshold() {
         BadRacer r = new BadRacer();
-        assertThat(r.doM(3)).isFalse();
+        assertThat(r.canMove(3)).isFalse();
     }
 
-    @DisplayName("테스트3")
+    @DisplayName("이름이 1자 이상 5자 이하이면 유효하다")
     @Test
-    void t3() {
+    void validNameWithinMaxLength() {
         BadRacer r = new BadRacer();
-        assertThat(r.chk("kim")).isTrue();
+        assertThat(r.isValidName("kim")).isTrue();
     }
 
-    @DisplayName("테스트4")
+    @DisplayName("이름이 5자를 초과하면 유효하지 않다")
     @Test
-    void t4() {
+    void invalidNameExceedingMaxLength() {
         BadRacer r = new BadRacer();
-        assertThat(r.chk("abcdef")).isFalse();
+        assertThat(r.isValidName("abcdef")).isFalse();
     }
 
-    @DisplayName("테스트5")
+    @DisplayName("가장 멀리 이동한 자동차가 우승자다")
     @Test
-    void t5() {
-        BadRacer r = new BadRacer();
-        Map<String, Integer> c = new LinkedHashMap<>();
-        c.put("kim", 3);
-        c.put("lee", 2);
-        c.put("park", 3);
-        List<String> w = r.doW(c);
+    void getWinnerWithMaxDistance() {
+        BadRacer racer = new BadRacer();
+        Map<String, Integer> car = new LinkedHashMap<>();
+        car.put("kim", 3);
+        car.put("lee", 2);
+        car.put("park", 3);
+        List<String> w = racer.getWinners(car);
         assertThat(w).containsExactlyInAnyOrder("kim", "park");
     }
 }
