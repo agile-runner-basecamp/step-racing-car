cfa5f02f34c58d28500d364b25b7afc00ab099b0
chlwlstj98 <c55822605@gmail.com>
2026-04-08 17:37:50 +0900

    [ECS-45] feat: 자동차 이름을 포장하는 CarName VO 추가

---
+++ java/step5-value-object/src/main/java/badcode/CarName.java
@@ -0,0 +1,33 @@
+package badcode;
+
+import java.util.Objects;
+
+public class CarName {
+    private final String name;
+
+    private CarName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public static CarName of(String name) {
+        return new CarName(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
