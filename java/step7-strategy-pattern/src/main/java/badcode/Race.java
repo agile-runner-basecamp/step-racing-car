f7e80f5873c64810b70244ef039456206cc55d66
chlwlstj98 <c55822605@gmail.com>
2026-04-12 15:33:05 +0900

    [ECS-45] refactor: Race에서 Random 하드코딩을 제거하고 MovingStrategy를 주입받도록 변경

--- java/step7-strategy-pattern/src/main/java/badcode/Race.java
+++ java/step7-strategy-pattern/src/main/java/badcode/Race.java
@@ -2,24 +2,23 @@ package badcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class HardcodedRace {
+public class Race {
     private List<RaceCar> cars;
+    List<MovingStrategy> movingStrategies;
 
-    public HardcodedRace(List<String> names) {
+    public Race(List<String> names, List<MovingStrategy> movingStrategies) {
         this.cars = new ArrayList<>();
         for (String name : names) {
             this.cars.add(new RaceCar(name));
         }
+        this.movingStrategies = movingStrategies;
     }
 
     // 😱 Random이 메서드 내부에 하드코딩 — 테스트에서 결과를 예측할 수 없음!
     public void playRound() {
-        Random random = new Random();
-        for (RaceCar car : cars) {
-            int value = random.nextInt(10);
-            car.move(value);
+        for (int i = 0; i < cars.size(); i++) {
+            cars.get(i).move(movingStrategies.get(i).getValue());
         }
     }
 
